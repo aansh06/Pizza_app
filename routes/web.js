82564657f0615c75e7506111cebbd87d73278141
aansh06 <aansh0611@gmail.com>
2023-08-09 16:49:54 +0530

    cart page completed

---
+++ routes/web.js
@@ -0,0 +1,16 @@
+const homeController = require('../app/http/controllers/homeController')
+const cartController = require('../app/http/controllers/customers/cartController')
+const authController = require('../app/http/controllers/authController')
+
+
+function initRoutes(app) {
+    app.get('/',homeController().index)
+    app.get('/cart',cartController().index)   
+    app.get('/login',authController().login)   
+    app.get('/register',authController().register)  
+
+    app.post('/update-cart',cartController().update)
+}
+
+
+module.exports = initRoutes;
--- server.js
+++ server.js
@@ -1,16 +1,62 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
-
+const { default: mongoose, mongo } = require("mongoose")   //const mongoose = require(mongoose)
+//const session = require("express-session")
 const PORT = process.env.PORT || 3000
+const session = require('express-session')
+const flash=require('express-flash')
+const MongoDbStore =  require('connect-mongo')(session)
+//.... Database Connection 
+const url = 'mongodb://localhost/pizza'
+mongoose.connect(url,
+  {
+    useNewUrlParser: true,
+    //useCreateIndex:true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+const connection= mongoose.connection;
+connection.once('open',()=> {
+  console.log('Database connected..');
+}).on('error',(err) => {
+  console.log('Connection failed..')
+  console.log(err);
+});
 
+
+// ....Session store
+let mongoStore= new MongoDbStore({
+  mongooseConnection: connection,
+  collection:'sessions'
+})
+
+
+// .... Session Config
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized:false,
+  store:mongoStore,
+  cookie:{maxAge: 1000*60*60*24} //24hrs
+}))
+
+
+app.use(flash())
 
   //.... Assets
 app.use(express.static('public'))
+app.use(express.json())
 
-
+//.... global middleware
+app.use((req,res,next)=>{
+  res.locals.session = req.session
+  next()
+})
 
 
 
@@ -20,23 +66,7 @@ app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine','ejs')
 
 
-app.get('/',(req, res)=>{
-  res.render('home')
-})
-
-app.get('/cart',(req,res)=>{
-  res.render('customers/cart')
-})
-
-
-app.get('/login',(req,res)=>{
-  res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-  res.render('auth/register')
-})
-
+require('./routes/web.js')(app)
 
 app.listen(PORT , ()=>{
     console.log(`Listening on port ${PORT}`)
